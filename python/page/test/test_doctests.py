15d129132a22b17965e589986167820b37b50177
Mike DeLaurentis <delaurentis@gmail.com>
2012-10-18 17:14:40 -0400

    Add doctests

---
+++ python/page/test/test_doctests.py
@@ -0,0 +1,13 @@
+import sys, os
+
+sys.path.insert(0, os.path.dirname(__file__) + "/../..")
+
+import unittest
+import doctest
+import page
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(page))
+    return tests
+
+unittest.main()
