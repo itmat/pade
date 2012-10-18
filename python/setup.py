ee03af06d8614a33d57a35ba51bcc393e8874bc9
Mike DeLaurentis <delaurentis@gmail.com>
2012-10-18 17:14:21 -0400

    Add setup.py

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PaGE',
+      version='6.0',
+      author='Mike DeLaurentis',
+      author_email='delaurentis@gmail.com',
+      url='https://github.com/mdelaurentis/fasta-parser',
+#      scripts=['parse_fasta'],
+      py_modules=['page'],
+      packages=['page'])
