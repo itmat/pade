d99de2b97513f4530bd7fc0b6d4dc6c50bdde7c4
Mike DeLaurentis <delaurentis@gmail.com>
2013-01-09 14:57:51 -0500

    Fix profile script

--- python/profile.py
+++ python/profile.py
@@ -1,6 +1,6 @@
 import sys
-import page.ui
+import page
 import cProfile
 
-sys.argv = ['foo', 'run', 'sample_data/4_class_testdata_header1.txt']
-cProfile.run('page.ui.main()', 'run_prof')
+sys.argv = ['foo', 'run', '--directory', 'profile/new_1000']
+cProfile.run('page.main()', 'run_prof')
