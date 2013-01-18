a97ca476dd7f66723018e49c138fc05c03bb6536
Mike DeLaurentis <delaurentis@gmail.com>
2013-01-18 10:07:09 -0500

    Split profiling out into performance.py

---
+++ python/page/performance.py
@@ -0,0 +1,82 @@
+import contextlib
+import resource
+import numpy as np
+
+EVENTS = []
+
+@contextlib.contextmanager
+def profiling(label):
+    pre_maxrss = maxrss()
+    EVENTS.append(('enter', label, maxrss()))
+    yield
+    post_maxrss = maxrss()
+    EVENTS.append(('exit', label, maxrss()))
+
+def profiled(method):
+
+    def wrapped(*args, **kw):
+        with profiling(method.__name__):
+            return method(*args, **kw)
+
+    return wrapped
+
+def maxrss():
+    """Return the current maximum RSS allocation in GB."""
+    bytes = float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+    return bytes / 1000000000.0
+
+
+def walk_profile(profile_log=EVENTS):
+    stack = []
+    events = []
+    order = 0
+    for entry in profile_log:
+        (event, label, maxrss) = entry
+        if event == 'enter':
+            stack.append({
+                    'order'      : order,
+                    'depth'      : len(stack),
+                    'label'      : label,
+                    'maxrss_pre' : maxrss })
+            order += 1
+        elif event == 'exit':
+            entry = stack.pop()
+            if entry['label'] != label:
+                raise ProfileStackException(
+                    "Expected to pop {0}, got {1} instead".format(
+                        label, entry['label']))
+            entry['maxrss_post'] = maxrss
+            events.append(entry)
+        else:
+            raise ProfileStackException(
+                "Unknown event " + event)
+
+    recs = [(e['depth'], 
+             e['order'],
+             e['label'],
+             e['maxrss_pre'], 
+             e['maxrss_post'],
+             0.0,
+             0.0)
+            for e in events]
+
+    dtype=[('depth', int),
+           ('order', int),
+           ('label', 'S100'),
+           ('maxrss_pre', float),
+           ('maxrss_post', float),
+           ('maxrss_diff', float),
+           ('maxrss_diff_percent', float)]
+
+    table = np.array(recs, dtype)
+
+    table['maxrss_diff'] = table['maxrss_post'] - table['maxrss_pre']
+    
+    table['maxrss_diff_percent'] = table['maxrss_diff']
+    if len(table['maxrss_post']) > 0:
+        table['maxrss_diff_percent'] /= max(table['maxrss_post'])
+    table.sort(order=['order'])
+    return table
+
+    
+
