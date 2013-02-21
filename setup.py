a6cf3093fab133ab373db23631ccac2c467c1c82
Mike DeLaurentis <delaurentis@gmail.com>
2013-02-21 14:26:00 -0500

    Change version to 0.2.0a2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pade',
-      version='0.2.0a1',
+      version='0.2.0a2',
       author='Mike DeLaurentis',
       author_email='midel@upenn.edu',
       url='https://github.com/itmat/pade',
