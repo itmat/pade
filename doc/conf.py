f1d41ffbb63406044643c462c4e86c6046a2f718
Mike DeLaurentis <delaurentis@gmail.com>
2013-02-27 14:12:09 -0500

    Change version to 0.2.0a3

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PaGE'
+project = u'PADE'
 copyright = u'2013, University of Pennsylvania'
 
 # The version info for the project you're documenting, acts as replacement for
