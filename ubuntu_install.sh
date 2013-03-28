9958de1943ed9de1397644e3d93be6039f440093
Mike DeLaurentis <delaurentis@gmail.com>
2013-03-28 15:35:37 -0400

    Add to ubuntu install

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,2 +1,6 @@
 # AMI: ami-3fec7956
-apt-get install git python-numpy python-scipy python-matplotlib python-h5py python-nose redis-server
+apt-get install git python-numpy python-scipy python-matplotlib python-h5py python-nose redis-server python-setuptools
+wget https://github.com/itmat/pade/archive/master.tar.gz
+tar zxvf master.tar.gz
+cd pade-master
+python setup.py install
