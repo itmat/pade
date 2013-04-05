bd611668eea3f9f70ad71428520db78caae835c3
Mike DeLaurentis <delaurentis@gmail.com>
2013-04-05 15:09:46 +0000

    Working on GLM timing script

--- pade/tools/time_glm.py
+++ pade/tools/time_glm.py
@@ -1,9 +1,17 @@
+from __future__ import print_function
+
 import numpy as np
 import statsmodels.api as sm
 from collections import namedtuple
+import time
 
+def time_fn(f, *args, **kwargs):
+    start = time.time()
+    res = f(*args, **kwargs)
+    end = time.time()
+    return (end - start, res)
 
-class OldResults(object):
+class GlmResults(object):
     def __init__(self, y, x, family, contrast, models, fitteds, fs):
         self.y = y
         self.x = x
@@ -43,15 +51,31 @@ def old_glm(y, x, family, contrast):
         fitteds.append(fitted)
         fs.append(f)
 
-    return OldResults(y, x, family, contrast, models, fitteds, fs)
+    return GlmResults(y, x, family, contrast, models, fitteds, fs)
+
+def new_glm(y, x, family, contrast):
+    models = []
+    fitteds = []
+    fs = []
+    for row in y:
+        model = sm.GLM(row, x, family)
+        fitted = model.fit()
+        f = fitted.f_test(contrast)
+        models.append(model)
+        fitteds.append(fitted)
+        fs.append(f)
+
+    return GlmResults(y, x, family, contrast, models, fitteds, fs)
+
+
 
 def main():
-    y = np.random.poisson(size=(10, 24))
+    y = np.random.poisson(size=(100, 24))
     
-    for i in range(3):
+    for i in range(30):
         y[i, :12] = y[i, :12] * np.random.poisson()
 
-    for i in range(3, 6):
+    for i in range(30, 60):
         y[i, 12:] = y[i, 12:] * np.random.poisson()
 
     x = np.zeros((24, 2), int)
@@ -60,12 +84,15 @@ def main():
 
     contrast = [0, 1]
 
-    old_res = old_glm(y, x, sm.families.Poisson(), contrast)
+    (old_time, old_res) = time_fn(old_glm, y, x, sm.families.Poisson(), contrast)
+    (new_time, new_res) = time_fn(new_glm, y, x, sm.families.Poisson(), contrast)
 
-    print old_res.params
-    print old_res.fittedvalues
-    print old_res.weights
-    print old_res.f_values
+    np.testing.assert_almost_equal(old_res.params, new_res.params)
+    np.testing.assert_almost_equal(old_res.fittedvalues, new_res.fittedvalues)
+    np.testing.assert_almost_equal(old_res.weights, new_res.weights)
+    np.testing.assert_almost_equal(old_res.f_values, new_res.f_values)
+
+    print(old_time, new_time)
 
 if __name__ == '__main__':
     main()
